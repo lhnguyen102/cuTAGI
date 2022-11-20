054c0337e0bfe5af68789595dcb9963234d1c727
Luong-Ha Nguyen <luongha.nguyen@gmail.com>
2022-11-20 18:45:28 -0500

    install cuda inside cibuildwheel

--- action_scripts/install_cuda_ubuntu.sh
+++ action_scripts/install_cuda_ubuntu.sh
@@ -1,21 +1,19 @@
+
 # @todo - better / more robust parsing of inputs from env vars.
 ## -------------------
 ## Constants
 ## -------------------
 
+# @todo - apt repos/known supported versions?
+
+# @todo - GCC support matrix?
+
 # List of sub-packages to install.
-# @todo - pass this in from outside the script? 
+# @todo - pass this in from outside the script?
 # @todo - check the specified subpackages exist via apt pre-install?  apt-rdepends cuda-9-0 | grep "^cuda-"?
 
-# Ideally choose from the list of meta-packages to minimise variance between cuda versions (although it does change too). Some of these packages may not be availble pre cuda 10.
-CUDA_PACKAGES_IN=(
-    "cuda-compiler"
-    "cuda-cudart-dev"
-    "cuda-nvtx"
-    "cuda-nvrtc-dev"
-    "libcurand-dev" # 11-0+
-    "cuda-cccl" # 11.4+, provides cub and thrust. On 11.3 knwon as cuda-thrust-11-3
-)
+# Ideally choose from the list of meta-packages to minimise variance between cuda versions (although it does change too)
+CUDA_PACKAGES_IN=("command-line-tools" "libraries-dev")
 
 ## -------------------
 ## Bash functions
@@ -54,7 +52,8 @@ CUDA_MAJOR=$(echo "${CUDA_VERSION_MAJOR_MINOR}" | cut -d. -f1)
 CUDA_MINOR=$(echo "${CUDA_VERSION_MAJOR_MINOR}" | cut -d. -f2)
 CUDA_PATCH=$(echo "${CUDA_VERSION_MAJOR_MINOR}" | cut -d. -f3)
 # use lsb_release to find the OS.
-UBUNTU_VERSION=$(lsb_release -sr)
+# UBUNTU_VERSION=$(lsb_release -sr)
+UBUNTU_VERSION = ${ubuntu_ver}
 UBUNTU_VERSION="${UBUNTU_VERSION//.}"
 
 echo "CUDA_MAJOR: ${CUDA_MAJOR}"
@@ -79,54 +78,41 @@ if [ -z ${UBUNTU_VERSION} ]; then
 fi
 
 
+## ---------------------------
+## GCC studio support check?
+## ---------------------------
+
+# @todo
+
 ## -------------------------------
 ## Select CUDA packages to install
 ## -------------------------------
 CUDA_PACKAGES=""
 for package in "${CUDA_PACKAGES_IN[@]}"
-do : 
+do :
     # @todo This is not perfect. Should probably provide a separate list for diff versions
     # cuda-compiler-X-Y if CUDA >= 9.1 else cuda-nvcc-X-Y
-    if [[ "${package}" == "cuda-nvcc" ]] && version_ge "$CUDA_VERSION_MAJOR_MINOR" "9.1" ; then
-        package="cuda-compiler"
-    elif [[ "${package}" == "cuda-compiler" ]] && version_lt "$CUDA_VERSION_MAJOR_MINOR" "9.1" ; then
-        package="cuda-nvcc"
-    # CUB/Thrust  are packages in cuda-thrust in 11.3, but cuda-cccl in 11.4+
-    elif [[ "${package}" == "cuda-thrust" || "${package}" == "cuda-cccl" ]]; then
-        # CUDA cuda-thrust >= 11.4
-        if version_ge "$CUDA_VERSION_MAJOR_MINOR" "11.4" ; then
-            package="cuda-cccl"
-        # Use cuda-thrust > 11.2
-        elif version_ge "$CUDA_VERSION_MAJOR_MINOR" "11.3" ; then
-            package="cuda-thrust"
-        # Do not include this pacakge < 11.3
-        else
-            continue
-        fi
-    fi
-    # CUDA 11+ includes lib* / lib*-dev packages, which if they existed previously where cuda-cu*- / cuda-cu*-dev-
-    if [[ ${package} == libcu* ]] && version_lt "$CUDA_VERSION_MAJOR_MINOR" "11.0" ; then
-        package="${package/libcu/cuda-cu}"
+    if [[ "${package}" == "nvcc" ]] && version_ge "$CUDA_VERSION_MAJOR_MINOR" "9.1" ; then
+        package="compiler"
+    elif [[ "${package}" == "compiler" ]] && version_lt "$CUDA_VERSION_MAJOR_MINOR" "9.1" ; then
+        package="nvcc"
     fi
     # Build the full package name and append to the string.
-    CUDA_PACKAGES+=" ${package}-${CUDA_MAJOR}-${CUDA_MINOR}"
+    CUDA_PACKAGES+=" cuda-${package}-${CUDA_MAJOR}-${CUDA_MINOR}"
 done
 echo "CUDA_PACKAGES ${CUDA_PACKAGES}"
 
 ## -----------------
 ## Prepare to install
 ## -----------------
-CPU_ARCH="x86_64"
+
 PIN_FILENAME="cuda-ubuntu${UBUNTU_VERSION}.pin"
-PIN_URL="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${UBUNTU_VERSION}/${CPU_ARCH}/${PIN_FILENAME}"
-# apt keyring package now available https://developer.nvidia.com/blog/updating-the-cuda-linux-gpg-repository-key/
-KERYRING_PACKAGE_FILENAME="cuda-keyring_1.0-1_all.deb"
-KEYRING_PACKAGE_URL="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${UBUNTU_VERSION}/${CPU_ARCH}/${KERYRING_PACKAGE_FILENAME}"
-REPO_URL="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${UBUNTU_VERSION}/${CPU_ARCH}/"
+PIN_URL="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${UBUNTU_VERSION}/x86_64/${PIN_FILENAME}"
+APT_KEY_URL="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${UBUNTU_VERSION}/x86_64/3bf863cc.pub"
+REPO_URL="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${UBUNTU_VERSION}/x86_64/"
 
 echo "PIN_FILENAME ${PIN_FILENAME}"
 echo "PIN_URL ${PIN_URL}"
-echo "KEYRING_PACKAGE_URL ${KEYRING_PACKAGE_URL}"
 echo "APT_KEY_URL ${APT_KEY_URL}"
 
 ## -----------------
@@ -143,8 +129,8 @@ has_sudo=false
 if command -v sudo &> /dev/null ; then
     has_sudo=true
 fi
-# Decide if we can proceed or not (root or sudo is required) and if so store whether sudo should be used or not. 
-if [ "$is_root" = false ] && [ "$has_sudo" = false ]; then 
+# Decide if we can proceed or not (root or sudo is required) and if so store whether sudo should be used or not.
+if [ "$is_root" = false ] && [ "$has_sudo" = false ]; then
     echo "Root or sudo is required. Aborting."
     exit 1
 elif [ "$is_root" = false ] ; then
@@ -159,7 +145,7 @@ fi
 echo "Adding CUDA Repository"
 wget ${PIN_URL}
 $USE_SUDO mv ${PIN_FILENAME} /etc/apt/preferences.d/cuda-repository-pin-600
-wget ${KEYRING_PACKAGE_URL} && ${USE_SUDO} dpkg -i ${KERYRING_PACKAGE_FILENAME} && rm ${KERYRING_PACKAGE_FILENAME}
+$USE_SUDO apt-key adv --fetch-keys ${APT_KEY_URL}
 $USE_SUDO add-apt-repository "deb ${REPO_URL} /"
 $USE_SUDO apt-get update
 
@@ -170,7 +156,6 @@ if [[ $? -ne 0 ]]; then
     echo "CUDA Installation Error."
     exit 1
 fi
-
 ## -----------------
 ## Set environment vars / vars to be propagated
 ## -----------------
@@ -178,12 +163,12 @@ fi
 CUDA_PATH=/usr/local/cuda-${CUDA_MAJOR}.${CUDA_MINOR}
 echo "CUDA_PATH=${CUDA_PATH}"
 export CUDA_PATH=${CUDA_PATH}
+
+
+# Quick test. @temp
 export PATH="$CUDA_PATH/bin:$PATH"
 export LD_LIBRARY_PATH="$CUDA_PATH/lib:$LD_LIBRARY_PATH"
-export LD_LIBRARY_PATH="$CUDA_PATH/lib64:$LD_LIBRARY_PATH"
-# Check nvcc is now available.
 nvcc -V
-
 
 # If executed on github actions, make the appropriate echo statements to update the environment
 if [[ $GITHUB_ACTIONS ]]; then
@@ -192,5 +177,4 @@ if [[ $GITHUB_ACTIONS ]]; then
     echo "CUDA_PATH=${CUDA_PATH}" >> $GITHUB_ENV
     echo "${CUDA_PATH}/bin" >> $GITHUB_PATH
     echo "LD_LIBRARY_PATH=${CUDA_PATH}/lib:${LD_LIBRARY_PATH}" >> $GITHUB_ENV
-    echo "LD_LIBRARY_PATH=${CUDA_PATH}/lib64:${LD_LIBRARY_PATH}" >> $GITHUB_ENV
 fi
