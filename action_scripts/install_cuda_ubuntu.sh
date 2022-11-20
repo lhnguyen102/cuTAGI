d1948e61e0e195ce0a10139ed4a7c3d2ed31f728
Luong-Ha Nguyen <luongha.nguyen@gmail.com>
2022-11-20 18:10:31 -0500

    install cuda inside cibuildwheel

--- action_scripts/install_cuda_ubuntu.sh
+++ action_scripts/install_cuda_ubuntu.sh
@@ -3,5 +3,21 @@ set -x
 yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
 yum install --setopt=obsoletes=0 -y cuda-nvcc-11-2-11.2.152-1 cuda-cudart-devel-11-2-11.2.152-1
 ln -s cuda-11.2 /usr/local/cuda
-echo "PATH=/usr/local/cuda/bin:$PATH" >> $GITHUB_PATH
-echo "LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH" >> $GITHUB_ENV
+
+CUDA_PATH=/usr/local/cuda
+echo "CUDA_PATH=${CUDA_PATH}"
+export CUDA_PATH=${CUDA_PATH}
+
+# Quick test. @temp
+export PATH="$CUDA_PATH/bin:$PATH"
+export LD_LIBRARY_PATH="$CUDA_PATH/lib:$LD_LIBRARY_PATH"
+nvcc -V
+
+# If executed on github actions, make the appropriate echo statements to update the environment
+if [[ $GITHUB_ACTIONS ]]; then
+    # Set paths for subsequent steps, using ${CUDA_PATH}
+    echo "Adding CUDA to CUDA_PATH, PATH and LD_LIBRARY_PATH"
+    echo "CUDA_PATH=${CUDA_PATH}" >> $GITHUB_ENV
+    echo "${CUDA_PATH}/bin" >> $GITHUB_PATH
+    echo "LD_LIBRARY_PATH=${CUDA_PATH}/lib:${LD_LIBRARY_PATH}" >> $GITHUB_ENV
+fi
