1cc9ebf5dca33776698134b66cc85c36a2db194a
Luong-Ha Nguyen <luongha.nguyen@gmail.com>
2022-11-20 16:34:26 -0500

    install cuda inside cibuildwheel

--- action_scripts/install_cuda_ubuntu.sh
+++ action_scripts/install_cuda_ubuntu.sh
@@ -145,6 +145,8 @@ fi
 ## Install
 ## -----------------
 echo "Adding CUDA Repository"
+$USE_SUDO apt-get update
+$USE_SUDO apt install wget
 wget ${PIN_URL}
 $USE_SUDO mv ${PIN_FILENAME} /etc/apt/preferences.d/cuda-repository-pin-600
 $USE_SUDO apt-key adv --fetch-keys ${APT_KEY_URL}
