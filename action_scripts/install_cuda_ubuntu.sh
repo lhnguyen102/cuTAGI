506b6f7250c761ae39c66c4b9f3b642cb8151e54
Luong-Ha Nguyen <luongha.nguyen@gmail.com>
2022-11-20 17:49:37 -0500

    install cuda inside cibuildwheel

--- action_scripts/install_cuda_ubuntu.sh
+++ action_scripts/install_cuda_ubuntu.sh
@@ -1,6 +1,11 @@
 set -e
 set -x
-yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
-yum install cuda
+sudo yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
+sudo yum clean all
+sudo yum -y install nvidia-driver-latest-dkms cuda
+sudo yum -y install cuda-drivers
+# yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
+# yum install cuda
+ln -s cuda-11.2 /usr/local/cuda
 export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
