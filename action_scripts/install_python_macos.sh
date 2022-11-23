5d82d8c50fc57e78957009f1b38dfec4ed06e279
Luong-Ha Nguyen <luongha.nguyen@gmail.com>
2022-11-23 08:45:39 -0500

    test python macos

--- action_scripts/install_python_macos.sh
+++ action_scripts/install_python_macos.sh
@@ -4,3 +4,9 @@ export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 export PATH=/usr/local/bin:/usr/local/sbin:$PATH
 brew install pyenv
 pyenv install ${PYTHON_VERSION_MACOS}
+if [[ $GITHUB_ACTIONS ]]; then
+    # Set paths for subsequent steps, using ${CUDA_PATH}
+    echo "Adding PYTHON to PATH"
+    echo "$/usr/local/opt/python/libexec/bin" >> $GITHUB_PATH
+    echo "$/usr/local/bin:/usr/local/sbin" >> $GITHUB_PATH
+fi
