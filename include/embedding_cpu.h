890cd9d932bef6f7460c7c2055878fe704c05637
Luong-Ha Nguyen <luongha.nguyen@gmail.com>
2023-08-27 18:06:36 -0400

    feat: adding bag multi embeddings

--- include/embedding_cpu.h
+++ include/embedding_cpu.h
@@ -4,7 +4,7 @@
 //               (CPU version)
 // Authors:      Luong-Ha Nguyen & James-A. Goulet
 // Created:      August 22, 2023
-// Updated:      August 23, 2023
+// Updated:      August 27, 2023
 // Contact:      luongha.nguyen@gmail.com & james.goulet@polymtl.ca
 // License:      This code is released under the MIT License.
 ///////////////////////////////////////////////////////////////////////////////
@@ -19,14 +19,22 @@
 struct EmbeddingProp {
     std::vector<int> num_categories, num_emb_sizes;
 };
+
 std::tuple<std::vector<float>, std::vector<float>> initialize_embedding_values(
     std::vector<int> num_classes, std::vector<int> num_weights, float scale,
     unsigned int *seed = nullptr);
+
 void forward(std::vector<float> &ma, std::vector<float> &mu_w,
              std::vector<float> &var_w, std::vector<int> &cat_sizes,
              std::vector<int> &emb_sizes, int num_cat, int batch_size,
-             int w_pos_in, int z_pos_in, int z_pos_out, int num_states,
+             int w_pos_in, int z_pos_in, int z_pos_out,
              std::vector<float> &mu_z, std::vector<float> &var_z);
-void param_backward(std::vector<float> &delta_mz, int z_pos_out,
-                    int num_states);
+
+void param_backward(std::vector<float> &ma, std::vector<float> &delta_mu,
+                    std::vector<float> &delta_var, std::vector<int> &cat_sizes,
+                    std::vector<int> &emb_sizes, int num_cat, int batch_size,
+                    int z_pos_in, int z_pos_out, int w_pos_in,
+                    std::vector<float> &delta_mu_w,
+                    std::vector<float> &delta_var_w);
+
 int calculate_embedding_size(int num_categories);
--- src/embedding_cpu.cpp
+++ src/embedding_cpu.cpp
@@ -4,7 +4,7 @@
 //               (CPU version)
 // Authors:      Luong-Ha Nguyen & James-A. Goulet
 // Created:      August 23, 2023
-// Updated:      August 23, 2023
+// Updated:      August 27, 2023
 // Contact:      luongha.nguyen@gmail.com & james.goulet@polymtl.ca
 // License:      This code is released under the MIT License.
 ///////////////////////////////////////////////////////////////////////////////
@@ -65,13 +65,15 @@ std::tuple<std::vector<float>, std::vector<float>> initialize_embedding_values(
     return {mu_emb, var_emb};
 }
 
+///////////////////////////////////////////////////////////////////////////////
+// Embedding Layer
+///////////////////////////////////////////////////////////////////////////////
 void forward(std::vector<float> &ma, std::vector<float> &mu_w,
              std::vector<float> &var_w, std::vector<int> &cat_sizes,
              std::vector<int> &emb_sizes, int num_cat, int batch_size,
-             int w_pos_in, int z_pos_in, int z_pos_out, int num_states,
+             int w_pos_in, int z_pos_in, int z_pos_out,
              std::vector<float> &mu_z, std::vector<float> &var_z)
-/*
- */
+/**/
 {
     for (int i = 0; i < batch_size; i++) {
         for (int j = 0; j < num_cat; j++) {
@@ -85,7 +87,65 @@ void forward(std::vector<float> &ma, std::vector<float> &mu_w,
     }
 }
 
-int calculate_embedding_size(int num_categories) {
+void param_backward(std::vector<float> &ma, std::vector<float> &delta_mu,
+                    std::vector<float> &delta_var, std::vector<int> &cat_sizes,
+                    std::vector<int> &emb_sizes, int num_cat, int batch_size,
+                    int z_pos_in, int z_pos_out, int w_pos_in,
+                    std::vector<float> &delta_mu_w,
+                    std::vector<float> &delta_var_w)
+/*
+ */
+{
+    for (int i = 0; i < batch_size; i++) {
+        for (int j = 0; j < num_cat; j++) {
+            int cat_idx = ma[j + i * batch_size + z_pos_in];
+            int emb_size = emb_sizes[j];
+            for (int k = 0; k < emb_size; k++) {
+                delta_mu_w[cat_idx * emb_size + k + w_pos_in] =
+                    delta_mu[k + z_pos_out];
+
+                delta_var_w[cat_idx * emb_size + k + w_pos_in] =
+                    delta_var[k + z_pos_out];
+            }
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Bag Embedding Layer
+///////////////////////////////////////////////////////////////////////////////
+void bag_forward(std::vector<float> &ma, std::vector<float> &mu_w,
+                 std::vector<float> &var_w, std::vector<int> &cat_sizes,
+                 std::vector<int> &emb_sizes, std::vector<int> &num_bags,
+                 std::vector<int> &bag_sizes, int num_cat, int batch_size,
+                 int w_pos_in, int z_pos_in, int z_pos_out,
+                 std::vector<float> &mu_z, std::vector<float> &var_z)
+/*
+ */
+{
+    for (int i = 0; i < batch_size; i++) {
+        for (int j = 0; j < num_cat; j++) {
+            for (int m = 0; m < num_bags[j]; m++) {
+                for (int n = 0; n < bag_sizes[m]; n++) {
+                    // TO BE DONE
+                    int cat_idx = ma[j + i * batch_size + z_pos_in];
+                    int emb_size = emb_sizes[j];
+                    for (int k = 0; k < emb_size; k++) {
+                        mu_z[k + z_pos_out] =
+                            mu_w[cat_idx * emb_size + k + w_pos_in];
+                        var_z[k + z_pos_out] =
+                            var_w[cat_idx * emb_size + k + w_pos_in];
+                    }
+                }
+            }
+        }
+    }
+}
+
+int calculate_embedding_size(int num_categories)
+/*
+ */
+{
     int emb_size = 1.6 * powf(num_categories, 0.56);
 
     return std::max(600, emb_size);
