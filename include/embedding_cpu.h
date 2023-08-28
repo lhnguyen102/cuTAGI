6ba4fd3370d50f1788aead6c9876cd537826bcdc
Luong-Ha Nguyen <luongha.nguyen@gmail.com>
2023-08-28 18:52:55 -0400

    feat: adding bag embeddings

--- include/embedding_cpu.h
+++ include/embedding_cpu.h
@@ -4,7 +4,7 @@
 //               (CPU version)
 // Authors:      Luong-Ha Nguyen & James-A. Goulet
 // Created:      August 22, 2023
-// Updated:      August 27, 2023
+// Updated:      August 28, 2023
 // Contact:      luongha.nguyen@gmail.com & james.goulet@polymtl.ca
 // License:      This code is released under the MIT License.
 ///////////////////////////////////////////////////////////////////////////////
@@ -30,11 +30,28 @@ void forward(std::vector<float> &ma, std::vector<float> &mu_w,
              int w_pos_in, int z_pos_in, int z_pos_out,
              std::vector<float> &mu_z, std::vector<float> &var_z);
 
-void param_backward(std::vector<float> &ma, std::vector<float> &delta_mu,
-                    std::vector<float> &delta_var, std::vector<int> &cat_sizes,
-                    std::vector<int> &emb_sizes, int num_cat, int batch_size,
-                    int z_pos_in, int z_pos_out, int w_pos_in,
-                    std::vector<float> &delta_mu_w,
+void param_backward(std::vector<float> &ma, std::vector<float> &var_w,
+                    std::vector<float> &delta_mu, std::vector<float> &delta_var,
+                    std::vector<int> &cat_sizes, std::vector<int> &emb_sizes,
+                    int num_cat, int batch_size, int z_pos_in, int z_pos_out,
+                    int w_pos_in, std::vector<float> &delta_mu_w,
                     std::vector<float> &delta_var_w);
+
+void bag_forward(std::vector<float> &mu_a, std::vector<float> &mu_w,
+                 std::vector<float> &var_w, std::vector<int> &cat_sizes,
+                 std::vector<int> &emb_sizes, std::vector<int> &num_bags,
+                 std::vector<int> &bag_sizes, int num_cat, int batch_size,
+                 int w_pos_in, int z_pos_in, int z_pos_out,
+                 std::vector<float> &mu_z, std::vector<float> &var_z);
+
+void bag_param_backward(std::vector<float> &mu_a, std::vector<float> &var_w,
+                        std::vector<float> &delta_mu,
+                        std::vector<float> &delta_var,
+                        std::vector<int> &cat_sizes,
+                        std::vector<int> &emb_sizes, std::vector<int> &num_bags,
+                        std::vector<int> &bag_sizes, int num_cat,
+                        int batch_size, int z_pos_in, int w_pos_in,
+                        int z_pos_out, std::vector<float> &delta_mu_w,
+                        std::vector<float> &delta_var_w);
 
 int calculate_embedding_size(int num_categories);
--- src/embedding_cpu.cpp
+++ src/embedding_cpu.cpp
@@ -4,7 +4,7 @@
 //               (CPU version)
 // Authors:      Luong-Ha Nguyen & James-A. Goulet
 // Created:      August 23, 2023
-// Updated:      August 27, 2023
+// Updated:      August 28, 2023
 // Contact:      luongha.nguyen@gmail.com & james.goulet@polymtl.ca
 // License:      This code is released under the MIT License.
 ///////////////////////////////////////////////////////////////////////////////
@@ -87,11 +87,11 @@ void forward(std::vector<float> &ma, std::vector<float> &mu_w,
     }
 }
 
-void param_backward(std::vector<float> &ma, std::vector<float> &delta_mu,
-                    std::vector<float> &delta_var, std::vector<int> &cat_sizes,
-                    std::vector<int> &emb_sizes, int num_cat, int batch_size,
-                    int z_pos_in, int z_pos_out, int w_pos_in,
-                    std::vector<float> &delta_mu_w,
+void param_backward(std::vector<float> &ma, std::vector<float> &var_w,
+                    std::vector<float> &delta_mu, std::vector<float> &delta_var,
+                    std::vector<int> &cat_sizes, std::vector<int> &emb_sizes,
+                    int num_cat, int batch_size, int z_pos_in, int z_pos_out,
+                    int w_pos_in, std::vector<float> &delta_mu_w,
                     std::vector<float> &delta_var_w)
 /*
  */
@@ -102,10 +102,12 @@ void param_backward(std::vector<float> &ma, std::vector<float> &delta_mu,
             int emb_size = emb_sizes[j];
             for (int k = 0; k < emb_size; k++) {
                 delta_mu_w[cat_idx * emb_size + k + w_pos_in] =
-                    delta_mu[k + z_pos_out];
+                    delta_mu[k + z_pos_out] *
+                    var_w[cat_idx * emb_size + k + w_pos_in];
 
                 delta_var_w[cat_idx * emb_size + k + w_pos_in] =
-                    delta_var[k + z_pos_out];
+                    delta_var[k + z_pos_out] *
+                    var_w[cat_idx * emb_size + k + w_pos_in];
             }
         }
     }
@@ -114,27 +116,146 @@ void param_backward(std::vector<float> &ma, std::vector<float> &delta_mu,
 ///////////////////////////////////////////////////////////////////////////////
 // Bag Embedding Layer
 ///////////////////////////////////////////////////////////////////////////////
-void bag_forward(std::vector<float> &ma, std::vector<float> &mu_w,
+void bag_forward(std::vector<float> &mu_a, std::vector<float> &mu_w,
                  std::vector<float> &var_w, std::vector<int> &cat_sizes,
                  std::vector<int> &emb_sizes, std::vector<int> &num_bags,
                  std::vector<int> &bag_sizes, int num_cat, int batch_size,
                  int w_pos_in, int z_pos_in, int z_pos_out,
                  std::vector<float> &mu_z, std::vector<float> &var_z)
-/*
+/* Forward pass for bag embedding layer.
+
+Args:
+    mu_a: Input mean stored the categorical variable in float
+    mu_w: Embedding mean
+    var_w: Embedding variance
+    cat_sizes: Number of classes for each categorical variable
+    emb_sizes: Embedding size for each categoricals
+    num_bags: Number of bags for each categorical variable
+    bag_sizes: Number of classes for each bag
+    num_cat: Number of categorical variables stored in node vector
+    batch_size: Number of observation in batch
+    w_pos_in: Start position of the embedding vector
+    z_pos_in: Start position of the input hidden state vector i.e., mu_a
+    z_pos_out: Start position of the output hidden state vector i.e., mu_z
+    mu_z: Mean of output hidden states
+    var_z: Variance of output hidden states
+
+Example:
+    A two categorical varaibles num_cat = 2 each having 5 and 6 classes. Also
+assuming emb_sizes = [2, 3], num_bags = [3, 1], and bag_sizes = [4, 4]
+
+    embedding vector
+    cat_1 = [
+        [0.13, 0.23], -> class 0
+        [0.53, 0.23], -> class 1
+        [0.32, 0.43], -> class 2
+        [0.22, 0.13], -> class 3
+        [0.72, 0.18], -> class 4
+    ]
+
+    cat_2 = [
+        [0.13, 0.23, 0.34], -> class 0
+        [0.53, 0.23, 0.53], -> class 1
+        [0.32, 0.43, 0.65], -> class 2
+        [0.22, 0.13, 0.75], -> class 3
+        [0.72, 0.18, 0.87], -> class 4
+        [0.52, 0.28, 0.17], -> class 5
+    ]
+    mu_a = [
+        [[0, 1, 3, 4], [2, 3, 1, 4], [4, 3, 2, 1]], # bag 0
+        [[1, 8, 9, 6]]                              # bag 1
+    ]
+
+    output size (3, 1)
  */
 {
     for (int i = 0; i < batch_size; i++) {
         for (int j = 0; j < num_cat; j++) {
-            for (int m = 0; m < num_bags[j]; m++) {
-                for (int n = 0; n < bag_sizes[m]; n++) {
-                    // TO BE DONE
-                    int cat_idx = ma[j + i * batch_size + z_pos_in];
-                    int emb_size = emb_sizes[j];
+            int bag = num_bags[j];
+            int emb_size = emb_sizes[j];
+
+            for (int m = 0; m < bag; m++) {
+                int bag_size = bag_sizes[m];
+                float sum_mu = 0.0f;
+                float sum_var = 0.0f;
+                for (int n = 0; n < bag_size; n++) {
+                    // Convert categorical index in each bag to integer. TODO:
+                    // need to avoid this conversion for computing performance
+                    int cat_idx = mu_a[n + m * bag_size + j * bag * bag_size +
+                                       i * num_cat * bag * bag_size + z_pos_in];
+
+                    // Sum over all embedding values for each bag
                     for (int k = 0; k < emb_size; k++) {
-                        mu_z[k + z_pos_out] =
-                            mu_w[cat_idx * emb_size + k + w_pos_in];
-                        var_z[k + z_pos_out] =
-                            var_w[cat_idx * emb_size + k + w_pos_in];
+                        sum_mu += mu_w[cat_idx * emb_size + k + w_pos_in];
+                        sum_var += var_w[cat_idx * emb_size + k + w_pos_in];
+                    }
+                }
+
+                // Average the embedding values. Output size (batch_size,
+                // num_cat, bag)
+                mu_z[m + j * bag + i * bag * num_cat + z_pos_out] =
+                    sum_mu / bag;
+                var_z[m + j * bag + i * bag * num_cat + z_pos_out] =
+                    sum_var / bag;
+            }
+        }
+    }
+}
+
+void bag_param_backward(std::vector<float> &mu_a, std::vector<float> &var_w,
+                        std::vector<float> &delta_mu,
+                        std::vector<float> &delta_var,
+                        std::vector<int> &cat_sizes,
+                        std::vector<int> &emb_sizes, std::vector<int> &num_bags,
+                        std::vector<int> &bag_sizes, int num_cat,
+                        int batch_size, int z_pos_in, int w_pos_in,
+                        int z_pos_out, std::vector<float> &delta_mu_w,
+                        std::vector<float> &delta_var_w)
+/*
+Args:
+    mu_a: Input mean stored the categorical variable in float
+    var_w: Embedding variance
+    delta_mu: Updating quantities for mean of the output layer
+    delta_var: Updating quantion for variance of the output layer
+    cat_sizes: Number of classes for each categorical variable
+    emb_sizes: Embedding size for each categoricals
+    num_bags: Number of bags for each categorical variable
+    bag_sizes: Number of classes for each bag
+    num_cat: Number of categorical variables stored in node vector
+    batch_size: Number of observation in batch
+    w_pos_in: Start position of the embedding vector
+    z_pos_in: Start position of the input hidden state vector i.e., mu_a
+    z_pos_out: Start postiion of the output hidden state vector i.e., mu_z
+    delta_mu_w: Updating quantities for mean of the embeddings
+    delta_var_w: Updating quantities for variance of the embeddings
+
+ */
+{
+    for (int i = 0; i < batch_size; i++) {
+        for (int j = 0; j < num_cat; j++) {
+            int bag = num_bags[j];
+            int emb_size = emb_sizes[j];
+
+            for (int m = 0; m < bag; m++) {
+                int bag_size = bag_sizes[m];
+                for (int n = 0; n < bag_size; n++) {
+                    // Convert categorical index in each bag to integer. TODO:
+                    // need to avoid this conversion for computing performance
+                    int cat_idx = mu_a[n + m * bag_size + j * bag * bag_size +
+                                       i * num_cat * bag * bag_size + z_pos_in];
+
+                    // Index for the updating quantities of the output layer
+                    int ino_idx = m + j * bag + i * bag * num_cat + z_pos_out;
+
+                    // Calculate the updating quanties for embeddings inside
+                    // each bag
+                    for (int k = 0; k < emb_size; k++) {
+                        // Index for embedding
+                        int w_idx = cat_idx * emb_size + k + w_pos_in;
+
+                        // Updating quantities for embedding
+                        delta_mu_w[w_idx] = delta_mu[ino_idx] * var_w[w_idx];
+                        delta_var_w[w_idx] = delta_var[ino_idx] * var_w[w_idx];
                     }
                 }
             }
