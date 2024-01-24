d5d2bedec4a76c4a3b8e8633777a2b51f4151c22
Luong-Ha Nguyen <luongha.nguyen@gmail.com>
2024-01-24 18:18:23 -0500

    feat: adding norm layer

---
+++ src/norm_layer.cpp
@@ -0,0 +1,10 @@
+///////////////////////////////////////////////////////////////////////////////
+// File:         norm_layer.cpp
+// Description:  ...
+// Authors:      Luong-Ha Nguyen & James-A. Goulet
+// Created:      January 24, 2024
+// Updated:      January 24, 2024
+// Contact:      luongha.nguyen@gmail.com & james.goulet@polymtl.ca
+// License:      This code is released under the MIT License.
+////////////////////////////////////////////////////////////////////////////////
+#pragma once
