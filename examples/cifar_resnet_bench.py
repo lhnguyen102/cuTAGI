3930d01fbdccf419523e65479d2cc8eebbeffbd5
Luong-Ha Nguyen <luongha.nguyen@gmail.com>
2024-08-23 17:49:15 -0400

    refactor: switching to 3conv

--- examples/cifar_resnet_bench.py
+++ examples/cifar_resnet_bench.py
@@ -229,8 +229,8 @@ def tagi_trainer(
     metric = HRCSoftmaxMetric(num_classes=10)
 
     # Resnet18
-    # net = TAGI_CNN_NET
-    net = resnet18_cifar10()
+    net = TAGI_CNN_NET
+    # net = resnet18_cifar10()
     net.to_device(device)
     # net.set_threads(10)
     out_updater = OutputUpdater(net.device)
