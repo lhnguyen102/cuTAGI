136c57f5fd026971be12c982ab52baec63192fc4
jamesgoulet <james.a.goulet@gmail.com>
2024-10-21 11:15:26 -0400

    Flip Act-Bnorm order & Add explicit gain parameter

--- examples/tagi_resnet_model.py
+++ examples/tagi_resnet_model.py
@@ -4,13 +4,13 @@ from pytagi.nn import (
     Conv2d,
     LayerBlock,
     Linear,
-    ReLU,
+    MixtureReLU,
     ResNetBlock,
     Sequential,
 )
 
 
-def make_layer_block(in_c: int, out_c: int, stride: int = 1, padding_type: int = 1):
+def make_layer_block(in_c: int, out_c: int, stride: int = 1, padding_type: int = 1, gain: float = 1):
     """Create a layer block for resnet 18"""
 
     return LayerBlock(
@@ -22,55 +22,68 @@ def make_layer_block(in_c: int, out_c: int, stride: int = 1, padding_type: int =
             stride=stride,
             padding=1,
             padding_type=padding_type,
+            gain_weight = gain,
+            gain_bias = gain,
         ),
         BatchNorm2d(out_c),
-        ReLU(),
-        Conv2d(out_c, out_c, 3, bias=False, padding=1),
+        MixtureReLU(),
+        Conv2d(out_c,
+               out_c, 3,
+               bias=False,
+               padding=1,
+               gain_weight = gain,
+               gain_bias = gain),
         BatchNorm2d(out_c),
+        MixtureReLU(),
     )
 
 
-def resnet18_cifar10() -> Sequential:
+def resnet18_cifar10(gain: float = 1) -> Sequential:
     """Resnet18 architecture for cifar10"""
     # 32x32
     initial_layers = [
-        Conv2d(3, 64, 3, bias=False, padding=1, in_width=32, in_height=32),
+        Conv2d(3, 64, 3,
+               bias=False,
+               padding=1,
+               in_width=32, in_height=32,
+               gain_weight = gain,
+               gain_bias = gain),
         BatchNorm2d(64),
-        ReLU(),
+        MixtureReLU(),
     ]
 
     resnet_layers = [
         # 32x32
-        ResNetBlock(make_layer_block(64, 64)),
-        ReLU(),
-        ResNetBlock(make_layer_block(64, 64)),
-        ReLU(),
+        ResNetBlock(make_layer_block(64, 64), gain=gain),
+        ResNetBlock(make_layer_block(64, 64), gain=gain),
         # 16x16
         ResNetBlock(
             make_layer_block(64, 128, 2, 2),
-            LayerBlock(Conv2d(64, 128, 2, bias=False, stride=2), BatchNorm2d(128)),
-        ),
-        ReLU(),
+            LayerBlock(Conv2d(64, 128, 2, bias=False, stride=2,
+                                gain_weight = gain,
+                                gain_bias = gain), BatchNorm2d(128)),
+        gain=gain),
         ResNetBlock(make_layer_block(128, 128)),
-        ReLU(),
         # 8x8
         ResNetBlock(
             make_layer_block(128, 256, 2, 2),
-            LayerBlock(Conv2d(128, 256, 2, bias=False, stride=2), BatchNorm2d(256)),
-        ),
-        ReLU(),
-        ResNetBlock(make_layer_block(256, 256)),
-        ReLU(),
+            LayerBlock(Conv2d(128, 256, 2, bias=False, stride=2,
+                                gain_weight = gain,
+                                gain_bias = gain), BatchNorm2d(256)),
+        gain=gain),
+        ResNetBlock(make_layer_block(256, 256), gain=gain),
         # 4x4
         ResNetBlock(
             make_layer_block(256, 512, 2, 2),
-            LayerBlock(Conv2d(256, 512, 2, bias=False, stride=2), BatchNorm2d(512)),
-        ),
-        ReLU(),
-        ResNetBlock(make_layer_block(512, 512)),
-        ReLU(),
+            LayerBlock(Conv2d(256, 512, 2, bias=False, stride=2,
+                                gain_weight = gain,
+                                gain_bias = gain), BatchNorm2d(512)),
+        gain=gain),
+        ResNetBlock(make_layer_block(512, 512), gain=gain),
     ]
 
-    final_layers = [AvgPool2d(4), Linear(512, 11)]
+    final_layers = [AvgPool2d(4), Linear(512, 11,
+                                gain_weight = gain,
+                                gain_bias = gain)]
 
     return Sequential(*initial_layers, *resnet_layers, *final_layers)
