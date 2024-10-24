Bugfix  - negative variance update (#95)
* Move gain inside pow() for param's variance initialiation

* Constrain parameter variances to >1E-5

* Update cap_factors

* Flip Act-Bnorm order & Add explicit gain parameter

* Update the classification example

* Update the cifar + resnet18 example

* Remove gain from classification.py

* Remive printout for Classification.py

* Fix gain parameter in the resnet architecture

* Latest changes

* Fix Norm_layer_cuda.cu bugs

* Switch order for MixtureReLU() and Barchnorm2d()

* Remove printout for variance cap

* code cleaning

* fix: pybind11

---------

Co-authored-by: Luong-Ha Nguyen <luongha.nguyen@gmail.com>

examples/cifar_resnet_bench.py
@@ -39,18 +39,18 @@ NORMALIZATION_STD = [0.2470, 0.2435, 0.2616]
 TAGI_CNN_NET = Sequential(
     # 32x32
     Conv2d(3, 32, 5, bias=False, padding=2, in_width=32, in_height=32),
-    BatchNorm2d(32),
     MixtureReLU(),
+    BatchNorm2d(32),
     AvgPool2d(2, 2),
     # 16x16
     Conv2d(32, 32, 5, bias=False, padding=2),
-    BatchNorm2d(32),
     MixtureReLU(),
+    BatchNorm2d(32),
     AvgPool2d(2, 2),
     # 8x8
     Conv2d(32, 64, 5, bias=False, padding=2),
-    BatchNorm2d(64),
     MixtureReLU(),
+    BatchNorm2d(64),
     AvgPool2d(2, 2),
     # 4x4
     Linear(64 * 4 * 4, 256),
@@ -226,7 +226,7 @@ def tagi_trainer(
 
     # Resnet18
     # net = TAGI_CNN_NET
-    net = resnet18_cifar10()
+    net = resnet18_cifar10(gain_w=0.15, gain_b=0.15)
     net.to_device(device)
     # net.set_threads(10)
     out_updater = OutputUpdater(net.device)
@@ -237,11 +237,12 @@ def tagi_trainer(
         (batch_size * metric.hrc_softmax.num_obs,), sigma_v**2, dtype=np.float32
     )
     pbar = tqdm(range(num_epochs), desc="Training Progress")
+    print_var = True
     for epoch in pbar:
         error_rates = []
         if epoch > 0:
             sigma_v = exponential_scheduler(
-                curr_v=sigma_v, min_v=0.2, decaying_factor=0.99, curr_iter=epoch
+                curr_v=sigma_v, min_v=0, decaying_factor=1, curr_iter=epoch
             )
             var_y = np.full(
                 (batch_size * metric.hrc_softmax.num_obs,),
@@ -252,6 +253,9 @@ def tagi_trainer(
         for x, labels in train_loader:
             # Feedforward and backward pass
             m_pred, v_pred = net(x)
+            if print_var: # Print prior predictive variance
+                print("Prior predictive -> E[v_pred] = ", np.average(v_pred), "+-", np.std(v_pred))
+                print_var = False
 
             # Update output layers based on targets
             y, y_idx, _ = utils.label_to_obs(labels=labels, num_classes=10)
@@ -365,7 +369,7 @@ def main(
     batch_size: int = 128,
     epochs: int = 50,
     device: str = "cuda",
-    sigma_v: float = 1,
+    sigma_v: float = 0.1,
 ):
     if framework == "torch":
         torch_trainer(batch_size=batch_size, num_epochs=epochs, device=device)

examples/classification.py
@@ -11,83 +11,82 @@ from pytagi.nn import (
     LayerNorm,
     Linear,
     OutputUpdater,
-    ReLU,
+    MixtureReLU,
     Sequential,
 )
 
 FNN = Sequential(
     Linear(784, 128),
-    ReLU(),
+    MixtureReLU(),
     Linear(128, 128),
-    ReLU(),
+    MixtureReLU(),
     Linear(128, 11),
 )
 
 FNN_BATCHNORM = Sequential(
-    Linear(784, 32),
-    ReLU(),
-    BatchNorm2d(32),
-    Linear(32,32),
-    ReLU(),
-    BatchNorm2d(32),
-    Linear(32, 11),
+    Linear(784, 100),
+    MixtureReLU(),
+    BatchNorm2d(100),
+    Linear(100, 100),
+    MixtureReLU(),
+    BatchNorm2d(100),
+    Linear(100, 11),
 )
 
 FNN_LAYERNORM = Sequential(
-    Linear(784, 32, bias=False),
-    ReLU(),
-    LayerNorm((32,)),
-    Linear(32, 32, bias=False),
-    ReLU(),
-    LayerNorm((32,)),
-    Linear(32, 11),
+    Linear(784, 100, bias=False),
+    MixtureReLU(),
+    LayerNorm((100,)),
+    Linear(100, 100, bias=False),
+    MixtureReLU(),
+    LayerNorm((100,)),
+    Linear(100, 11),
 )
 
 CNN = Sequential(
     Conv2d(1, 16, 4, padding=1, in_width=28, in_height=28),
-    ReLU(),
+    MixtureReLU(),
     AvgPool2d(3, 2),
     Conv2d(16, 32, 5),
-    ReLU(),
+    MixtureReLU(),
     AvgPool2d(3, 2),
     Linear(32 * 4 * 4, 100),
-    ReLU(),
+    MixtureReLU(),
     Linear(100, 11),
 )
 
 CNN_BATCHNORM = Sequential(
     Conv2d(1, 16, 4, padding=1, in_width=28, in_height=28, bias=False),
-    ReLU(),
+    MixtureReLU(),
     BatchNorm2d(16),
     AvgPool2d(3, 2),
     Conv2d(16, 32, 5, bias=False),
-    ReLU(),
+    MixtureReLU(),
     BatchNorm2d(32),
     AvgPool2d(3, 2),
     Linear(32 * 4 * 4, 100),
-    ReLU(),
+    MixtureReLU(),
     Linear(100, 11),
 )
 
 CNN_LAYERNORM = Sequential(
     Conv2d(1, 16, 4, padding=1, in_width=28, in_height=28, bias=False),
+    MixtureReLU(),
     LayerNorm((16, 27, 27)),
-    ReLU(),
     AvgPool2d(3, 2),
     Conv2d(16, 32, 5, bias=False),
+    MixtureReLU(),
     LayerNorm((32, 9, 9)),
-    ReLU(),
     AvgPool2d(3, 2),
     Linear(32 * 4 * 4, 100),
-    ReLU(),
+    MixtureReLU(),
     Linear(100, 11),
 )
 
 
-def main(num_epochs: int = 10, batch_size: int = 48, sigma_v: float = 2.0):
+def main(num_epochs: int = 10, batch_size: int = 1, sigma_v: float = 0.1):
     """
     Run classification training on the MNIST dataset using a custom neural model.
-
     Parameters:
     - num_epochs: int, number of epochs for training
     - batch_size: int, size of the batch for training
@@ -103,14 +102,13 @@ def main(num_epochs: int = 10, batch_size: int = 48, sigma_v: float = 2.0):
         y_file="data/mnist/t10k-labels-idx1-ubyte",
         num_images=10000,
     )
-
     # Hierachical Softmax
     metric = HRCSoftmaxMetric(num_classes=10)
 
     # Network configuration
-    net = FNN_BATCHNORM
-    net.to_device("cpu")
-    net.set_threads(48)
+    net = CNN
+    net.to_device("cuda")
+    #net.set_threads(16)
     out_updater = OutputUpdater(net.device)
 
     # Training
@@ -119,11 +117,15 @@ def main(num_epochs: int = 10, batch_size: int = 48, sigma_v: float = 2.0):
         (batch_size * metric.hrc_softmax.num_obs,), sigma_v**2, dtype=np.float32
     )
     pbar = tqdm(range(num_epochs), desc="Training Progress")
+    print_var = False
     for epoch in pbar:
         batch_iter = train_dtl.create_data_loader(batch_size=batch_size)
         for x, y, y_idx, label in batch_iter:
             # Feedforward and backward pass
             m_pred, v_pred = net(x)
+            if print_var: # Print prior predictive variance
+                print("Prior predictive -> E[v_pred] = ", np.average(v_pred), "+-", np.std(v_pred))
+                print_var = False
 
             # Update output layers based on targets
             out_updater.update_using_indices(