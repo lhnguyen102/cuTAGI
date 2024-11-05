d1f85e0fa1f66f1b3f25cd01ce237f705d0a81f3
Luong-Ha Nguyen <luongha.nguyen@gmail.com>
2024-11-05 08:38:50 -0500

    feat: adding logger

---
+++ include/custom_logger.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <stdexcept>
+#include <string>
+
+// Log Levels
+enum class LogLevel { INFO, WARNING, ERROR, DEBUG };
+
+class Logger {
+   public:
+    static void log_message(LogLevel level, const char* file, int line,
+                            const std::string& message);
+
+   private:
+    static std::string current_utc_time();
+
+    static std::string log_level_to_string(LogLevel level);
+};
+
+// Macro to simplify logging with automatic file and line number
+#define LOG(level, message) \
+    Logger::logMessage(level, __FILE__, __LINE__, message)
