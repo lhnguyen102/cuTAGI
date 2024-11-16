51591a240d1d01969f2c28f2d55b72b451c06ecb
Luong-Ha Nguyen <luongha.nguyen@gmail.com>
2024-11-16 11:23:03 -0500

    removed old norm layers

--- src/norm_layer_cuda.cu
+++
@@ -1,7 +0,0 @@
-#include "../include/config.h"
-#include "../include/cuda_error_checking.cuh"
-#include "../include/custom_logger.h"
-#include "../include/norm_layer.h"
-#include "../include/norm_layer_cuda.cuh"
-
-#define WARP_SIZE 32
