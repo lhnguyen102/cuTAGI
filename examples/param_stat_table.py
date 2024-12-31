b2608b34ef039feab9fc838142c4b0c09d1a2931
Luong-Ha Nguyen <luongha.nguyen@gmail.com>
2024-12-31 18:19:04 -0500

    modifying wandb log to mean and var values

--- examples/param_stat_table.py
+++ examples/param_stat_table.py
@@ -158,7 +158,7 @@ class ParamStatTable:
         rows = []
 
         for layer_name, snapshots in self.layer_updates_data.items():
-            row = self._gather_row(layer_name, snapshots, use_diff_metric=True)
+            row = self._gather_row(layer_name, snapshots, use_diff_metric=False)
             if row:
                 rows.append(row)
 
