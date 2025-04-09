611170f4d834f1ad5281db4220e5f6b998760f09
van-dai-vuong <99916615+van-dai-vuong@users.noreply.github.com>
2025-04-09 13:38:18 -0400

    Create test_customized_output_updater.py

---
+++ test/py_unit/test_customized_output_updater.py
@@ -0,0 +1,156 @@
+import os
+import sys
+import unittest
+
+import numpy as np
+
+import pytagi
+import pytagi.metric as metric
+from examples.data_loader import TimeSeriesDataloader
+from pytagi import Normalizer as normalizer
+from pytagi.nn import LSTM, SLSTM, Linear, OutputUpdater, Sequential, SLinear
+
+# path to binding code
+sys.path.append(
+    os.path.normpath(
+        os.path.join(os.path.dirname(__file__), "..", "..", "build")
+    )
+)
+
+TEST_CPU_ONLY = os.getenv("TEST_CPU_ONLY") == "1"
+
+
+def user_output_updater(
+    mu_obs: float,
+    var_obs: float,
+    jcb: list,
+    mu_output_z: list,
+    var_output_z: list,
+    batch_size: int,
+):
+    jcb = np.array(jcb, dtype=np.float32).reshape((-1, batch_size))
+    jcb = jcb[0, :]
+    mu_output_z = np.array(mu_output_z, dtype=np.float32).reshape(
+        (-1, batch_size)
+    )
+    mu_output_z = mu_output_z[0, :]
+    var_output_z = np.array(var_output_z, dtype=np.float32).reshape(
+        (-1, batch_size)
+    )
+    var_output_z = var_output_z[0, :]
+
+    tmp = jcb / (var_output_z + var_obs)
+    delta_mu = tmp * (mu_obs - mu_output_z)
+    delta_var = -tmp * jcb
+    return (
+        delta_mu,
+        delta_var,
+    )
+
+
+def lstm_test_runner(
+    model: Sequential,
+    input_seq_len: int = 4,
+    batch_size: int = 8,
+    use_cuda: bool = False,
+) -> float:
+    """Run training for time-series forecasting model"""
+    # Dataset
+    output_col = [0]
+    num_features = 1
+    output_seq_len = 1
+    seq_stride = 1
+
+    train_dtl = TimeSeriesDataloader(
+        x_file="data/toy_time_series/x_train_sin_data.csv",
+        date_time_file="data/toy_time_series/train_sin_datetime.csv",
+        output_col=output_col,
+        input_seq_len=input_seq_len,
+        output_seq_len=output_seq_len,
+        num_features=num_features,
+        stride=seq_stride,
+    )
+
+    model.to_device("cuda" if use_cuda else "cpu")
+
+    # -------------------------------------------------------------------------#
+    # Training
+    var_y = np.full((batch_size * len(output_col),), 0.02**2, dtype=np.float32)
+
+    for _ in np.arange(2):
+        mses = []
+        batch_iter = train_dtl.create_data_loader(batch_size, False)
+        for x, y in batch_iter:
+            # Feed forward
+            m_pred, _ = model(x)
+
+            (
+                model.input_delta_z_buffer.delta_mu,
+                model.input_delta_z_buffer.delta_var,
+            ) = user_output_updater(
+                mu_obs=y,
+                var_obs=var_y,
+                jcb=model.output_z_buffer.jcb,
+                mu_output_z=model.output_z_buffer.mu_a,
+                var_output_z=model.output_z_buffer.var_a,
+                batch_size=batch_size,
+            )
+            if model.device == "cuda":
+                model.delta_z_to_device()
+
+            # Feed backward
+            model.backward()
+            model.step()
+
+            # Training metric
+            pred = normalizer.unstandardize(
+                m_pred,
+                train_dtl.x_mean[output_col],
+                train_dtl.x_std[output_col],
+            )
+            obs = normalizer.unstandardize(
+                y, train_dtl.x_mean[output_col], train_dtl.x_std[output_col]
+            )
+            mse = metric.mse(pred, obs)
+            mses.append(mse)
+
+    return sum(mses) / len(mses)
+
+
+class SineSignalTest(unittest.TestCase):
+
+    def setUp(self):
+        self.threshold = 0.5
+
+    def test_lstm_CPU(self):
+        input_seq_len = 4
+        model = Sequential(
+            LSTM(1, 8, input_seq_len),
+            LSTM(8, 8, input_seq_len),
+            Linear(8 * input_seq_len, 1),
+        )
+        mse = lstm_test_runner(model, input_seq_len=input_seq_len)
+        self.assertLess(
+            mse, self.threshold, "Error rate is higher than threshold"
+        )
+
+    @unittest.skipIf(TEST_CPU_ONLY, "Skipping CUDA tests due to --cpu flag")
+    def test_lstm_CUDA(self):
+        if not pytagi.cuda.is_available():
+            self.skipTest("CUDA is not available")
+        input_seq_len = 4
+        model = Sequential(
+            LSTM(1, 8, input_seq_len),
+            LSTM(8, 8, input_seq_len),
+            Linear(8 * input_seq_len, 1),
+        )
+        mse = lstm_test_runner(
+            model, input_seq_len=input_seq_len, use_cuda=True
+        )
+        self.assertLess(
+            mse, self.threshold, "Error rate is higher than threshold"
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
